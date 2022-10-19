8bb8e46016ed69ff40cdb9b91d247b9ee3527aa9
PlanetNamekTech <sr23513@gmail.com>
2022-10-19 16:32:27 -0700

    going to use another project

--- running.py
+++ running.py
@@ -3,5 +3,11 @@ from openpyxl.workbook import Workbook
 
 #grab data from respective files
 df_excel = pd.read_excel('regions.xlsx')
-df_csv = pd.read_csv('Names.csv')
-df_txt = pd.read_csv('data.txt')
+df_csv = pd.read_csv('Names.csv', header=None)
+df_txt = pd.read_csv('data.txt', delimiter='\t')
+
+df_csv.columns = ['First', 'Last', 'Address', 'City', 'State', 'Area Code']
+#save the modified file as an excel file in this folder
+df_csv.to_excel('modified.xlsx')
+
+print(df_excel)
